885fbe6f7053f10ea8f8767c7323a582a008c42b
Geraldo Humberto <geraldohum@gmail.com>
2026-01-02 10:43:07 -0300

    Subindo os arquivos da aula

---
+++ aula1.py
@@ -0,0 +1,16 @@
+"""
+Este é um comentário de múltiplas linhas
+em Python. Pode ser usado para documentar
+
+conhecido como docstring.
+
+ideal pra escrever notas ou para explicar partes do código.
+"""
+'''
+Notas também podem ser escritas
+usando aspas simples triplas.
+'''
+# Permite escrever um comentário
+print(123) # Na frente
+# Abaixo
+print(456)
